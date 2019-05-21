ab6e37cbde34a6582bf01bb2694110a76c321801
skrskr66 <847020588@qq.com>
2019-05-21 23:25:02 +0800

    fourth

--- 2019.05.21/String2.cpp
+++ 2019.05.21/String2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -116,8 +117,39 @@ void test_string3()
   cout<<"2222" + str1<<endl;
 }
 
+void test_string4()
+{
+  string s;
+  getline(cin,s);
+  reverse(s.begin(),s.end());
+  //cout<<s<<endl;
+  string::iterator start = s.begin();
+  //cout<<*start<<endl;
+  string::iterator end = s.end();
+  while(*start != *end)
+  {
+    string::iterator end_word = start;
+    while(end_word != end && *end_word != ' ')
+    {
+      ++end_word;
+    }
+    //cout<<*end_word<<endl;打印验证此时在空白' '处
+      reverse(start,end_word);
+      if(end_word != end)
+      {
+        start = end_word + 1;
+      }
+      else
+      {
+        start = end_word;
+      }
+    
+  }
+  cout<<s<<endl;
+}
+
 int main()
 {
-  test_string2();
+  test_string4();
   return 0;
 }
